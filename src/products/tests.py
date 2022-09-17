f3f2ecf0336cf9c2db1252bf4733e5fabeebb077
Wael Ramadan <thebigguyis@gmail.com>
2022-09-17 21:51:05 +0300

    add product test

--- src/products/tests.py
+++ src/products/tests.py
@@ -1,3 +1,14 @@
+from unicodedata import category
 from django.test import TestCase
+from products.models import Category, Product
 
-# Create your tests here.
+
+class ProductsTest(TestCase):
+
+    def setUp(self):
+        cat_obj = Category.objects.create(name='test_category')
+        Product.objects.create(name='test_product', description='this is a test product', image='static/img/sample.jpg', category=cat_obj)
+
+    def test_get_product(self):
+        prod_obj = Product.objects.get(name='test_product')
+        self.assertEqual(prod_obj.name, 'test_product')
